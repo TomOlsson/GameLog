7a4652f4ae0c4bf533375195f3052790f1a0d6cb
TomOlsson <TomOlsson95@gmail.com>
2020-03-23 19:41:45 +0100

    Update GameLog.cs

--- GameLog.cs
+++ GameLog.cs
@@ -9,10 +9,10 @@ public class GameLog : MonoBehaviour
     public string secretKey;    //Key to Game
 
     //To post, follow this format. 
-    //Put whereevery you want in any class, just get the reference to GameLogObject correct
+    //Put whereever you want in any class, just get the reference to GameLogObject correct
     //string[][] postAttr = new string[2][];
-    //postAttr[0] = new string[] { column1, column2, ... };  //Column header
-    //postAttr[1] = new string[] { value1, value2, ... };  //Column values
+    //postAttr[0] = new string[] { column1, column2, ... };  	//Column names
+    //postAttr[1] = new string[] { value1, value2, ... };  	//Column values
     //GameLogObject('this', if this script is in the same object).GetComponent<GameLog>().LOG("t4b1e1Dxxx", postAttr);
 
     //We start by just getting the HighScores, this should be removed, when you are done setting up.
